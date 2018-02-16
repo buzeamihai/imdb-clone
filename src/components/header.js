b3cbe74cea44a0effbca5e37eeb27cf67c20e0a9
Cristutz <cristutz00@gmail.com>
2018-02-15 19:43:37 -0800

    with search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
+import Search from './searchComponent';
 
 
 class Header extends Component {
     render() {
         return (
             <header className="container">
-                <Link to="/" className="left brand-logo">Imdb Clone</Link>                    
+                <Link to="/" className="left brand-logo">Imdb Clone</Link> 
+                <Search />                   
                 <div className="row right">
-                    <Link to="/searchResults" className="col s4" >Search</Link>
                     <Link to="/register" className="col s4">Register</Link>
                     <Link to="/login" className="col s4">Log in</Link>
                 </div>            
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import  ReduxPromise  from 'redux-promise';
-import 'materialize-css/dist/css/materialize.min.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
