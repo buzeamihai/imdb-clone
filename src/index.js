494dba2ae9119d0867af3be1b0bb2ba4955573f6
Cristutz <cristutz00@gmail.com>
2018-02-20 20:23:14 -0800

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import { Provider} from 'react-redux';
-//import { createStore, applyMiddleware } from 'redux';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-//import  ReduxPromise  from 'redux-promise';
+import  ReduxPromise  from 'redux-promise';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import rootReducer from './reducers';
